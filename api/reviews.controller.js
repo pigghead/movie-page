7f0919ecb73008b8b2c030a57515179ae892ced7
Justin <justin.strongarms10@gmail.com>
2023-09-20 23:28:26 -0400

    added separation of concerns; router & controller

---
+++ api/reviews.controller.js
@@ -0,0 +1,86 @@
+import ReviewsDAO from '../dao/reviewsDAO.js';
+
+export default class ReviewsController {
+    static async apiPostReview(req, res, next) {  // assuming next is a best practice
+        try {
+            const movieId = req.body.movieId;  // body is the json part of the review
+            const review = req.body.review;
+            const user = req.body.user;
+
+            const reviewResponse = await ReviewsDAO.addReview(
+                movieId,
+                review,
+                user
+            );
+            res.json({status: 'success'});
+        } catch (e) {
+            res.status(500).json({error: e.message});
+        }
+    }
+
+    static async apiGetReview(req, res, next) {
+        try {
+            let id = req.params.id || {};  // params are what are found in the url (:id,etc)
+            let review = ReviewsDAO.getReview(id);
+            if(!review) {
+                res.status(404).json({error: 'Not found'});
+                return;
+            }
+            res.json(review)
+        } catch(e) {
+            console.log(`api, ${e}`);
+            res.status(500).json({error: e.message});
+        }
+    }
+
+    static async apiUpdateReview(req, res, next) {
+        try {
+            const reviewId = req.params.id;
+            const review = req.body.review;
+            const user = req.body.user;
+
+            const reviewResponse = ReviewsDAO.updateReview(
+                reviewId,
+                user,
+                review
+            );
+
+            var {error} = reviewResponse;
+            if(error) {
+                res.status(400).json({error});
+            }
+
+            if (reviewResponse === 0) {
+                throw new Error(
+                    'unable to update review'
+                );
+            }
+        } catch (e) {
+            res.status(500).json({error: e.message}); 
+        }
+    }
+
+    static async apiDeleteReview(req, res, next) {
+        try {
+            const reviewId = req.params.id;
+            const reviewResponse = await ReviewsDAO.deleteReview(reviewId);  // why put this in a const?
+            res.json({status: 'success'});
+        } catch(e) {
+            res.status(500).json({error: e.message});
+        }
+    }
+
+    static async apiGetReviews(req, res, next) {
+        try {
+            let id = req.params.id || {};
+            let reviews = await ReviewsDAO.getReviews(id);
+            if(!reviews) {
+                res.status(404).json({status: 'unable to get reviews for movie'});
+                return;
+            }
+            res.json(reviews);
+        } catch (e) {
+            res.status(500).json({error: e.message});
+        }
+    }
+}
--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import dotenv from 'dotenv';
 import app from './server.js';
 import mongodb, { MongoClient } from 'mongodb';
 //import Reviews from './dao/reviewsDAO.js';  // dao = data access object
+
+// sets up use for process.env variables (see .env)
 dotenv.config();
 
 const mongoClient = mongodb.MongoClient;
-const mongo_password = process.env.mongo_password;
 const mongo_username = process.env.mongo_username;
+const mongo_password = process.env.mongo_password;
+
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.rcqf5qq.mongodb.net/?retryWrites=true&w=majority`;
 const port = 8000;
-
-console.log(uri);
 
 mongoClient.connect(
     uri,
